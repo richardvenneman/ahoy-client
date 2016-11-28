4ac359765a01cfdf7bcd7332cda4a6e8eb90dc93
Richard Venneman <richardvenneman@me.com>
2016-11-28 11:20:31 +0100

    Add missing config attributes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 export default {
   urlPrefix: '',
   visitsUrl: '/ahoy/visits',
+  visitTtl: 4 * 60, // 4 hours
+  visitorTtl: 2 * 365 * 24 * 60, // 2 years
   eventsUrl: '/ahoy/events',
   cookieDomain: null,
   page: null,
